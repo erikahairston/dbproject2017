cdcf1323fb92fa9ef7770c05946e6df63105ac8d
Vincent Huang <vincent.vwh.huang@gmail.com>
2017-12-01 18:07:50 -0500

    Added Yelp data table definitions and business csv file

--- djangoProj/mysite/polls/admin.py
+++ djangoProj/mysite/polls/admin.py
@@ -3,6 +3,28 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question
 from .models import Airbnb_listing
+from .models import attribute
+from .models import business
+from .models import category
+from .models import checkin
+from .models import elite_years
+from .models import friend
+from .models import hours
+from .models import photo
+from .models import review
+from .models import tip
+from .models import user
 
 admin.site.register(Question)
 admin.site.register(Airbnb_listing)
+admin.site.register(attribute)
+admin.site.register(business)
+admin.site.register(category)
+admin.site.register(checkin)
+admin.site.register(elite_years)
+admin.site.register(friend)
+admin.site.register(hours)
+admin.site.register(photo)
+admin.site.register(review)
+admin.site.register(tip)
+admin.site.register(user)
--- djangoProj/mysite/polls/models.py
+++ djangoProj/mysite/polls/models.py
@@ -45,3 +45,94 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
+
+class business(models.Model):
+    id = models.CharField(max_length=22, primary_key=True)
+    name = models.CharField(max_length=255, null=True, default=None)
+    neighborhood = models.CharField(max_length=255, null=True, default=None)
+    address = models.CharField(max_length=255, null=True, default=None)
+    city = models.CharField(max_length=255, null=True, default=None)
+    state = models.CharField(max_length=255, null=True, default=None)
+    postal_code = models.CharField(max_length=255, null=True, default=None)
+    latitude = models.FloatField(null=True, default=None)
+    longitude = models.FloatField(null=True, default=None)
+    stars = models.FloatField(null=True, default=None)
+    review_count = models.IntegerField(null=True, default=None)
+    is_open = models.IntegerField(null=True, default=None)
+    def __str__(self):
+        return self.name
+
+class user(models.Model):
+    id = models.CharField(max_length=22, primary_key=True)
+    name = models.CharField(max_length=255, null=True, default=None)
+    review_count = models.IntegerField(null=True, default=None)
+    yelping_since = models.DateTimeField('date joined', null=True, default=None)
+    useful = models.IntegerField(null=True, default=None)
+    funny = models.IntegerField(null=True, default=None)
+    cool = models.IntegerField(null=True, default=None)
+    fans = models.IntegerField(null=True, default=None)
+    average_stars = models.FloatField(null=True, default=None)
+    compliment_hot = models.IntegerField(null=True, default=None)
+    compliment_more = models.IntegerField(null=True, default=None)
+    compliment_profile = models.IntegerField(null=True, default=None)
+    compliment_cute = models.IntegerField(null=True, default=None)
+    compliment_list = models.IntegerField(null=True, default=None)
+    compliment_note = models.IntegerField(null=True, default=None)
+    compliment_plain = models.IntegerField(null=True, default=None)
+    compliment_cool = models.IntegerField(null=True, default=None)
+    compliment_funny = models.IntegerField(null=True, default=None)
+    compliment_writer = models.IntegerField(null=True, default=None)
+    compliment_photos = models.IntegerField(null=True, default=None)
+    def __str__(self):
+        return self.name
+
+class attribute(models.Model):
+    business_id = models.ForeignKey(business, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=255, null=True, default=None)
+    value = models.TextField()
+
+class category(models.Model):
+    business_id = models.ForeignKey(business, on_delete=models.DO_NOTHING)
+    category = models.CharField(max_length=255, null=True, default=None)
+
+class checkin(models.Model):
+    business_id = models.ForeignKey(business, on_delete=models.DO_NOTHING)
+    date = models.CharField(max_length=255, null=True, default=None)
+    count = models.IntegerField(null=True, default=None)
+
+class elite_years(models.Model):
+    user_id = models.ForeignKey(user, on_delete=models.DO_NOTHING)
+    year = models.IntegerField(null=True, default=None)
+
+class friend(models.Model):
+    user_id = models.ForeignKey(user, on_delete=models.DO_NOTHING)
+    friend_id = models.CharField(max_length=22, null=True, default=None)
+
+class hours(models.Model):
+    hours = models.CharField(max_length=255, null=True, default=None)
+    business_id = models.ForeignKey(business, on_delete=models.DO_NOTHING)
+
+class photo(models.Model):
+    id = models.CharField(max_length=22, primary_key=True)
+    business_id = models.ForeignKey(business, on_delete=models.DO_NOTHING)
+    caption = models.CharField(max_length=255, null=True, default=None)
+    label = models.CharField(max_length=255, null=True, default=None)
+    
+class review(models.Model):
+    id = models.CharField(max_length=22, primary_key=True)
+    stars = models.IntegerField(null=True, default=None)
+    date = models.DateTimeField('date published', null=True, default=None)
+    text = models.TextField()
+    useful = models.IntegerField(null=True, default=None)
+    funny = models.IntegerField(null=True, default=None)
+    cool = models.IntegerField(null=True, default=None)
+    business_id = models.ForeignKey(business, on_delete=models.DO_NOTHING)
+    user_id = models.ForeignKey(user, on_delete=models.DO_NOTHING)
+
+class tip(models.Model):
+    user_id = models.ForeignKey(user, on_delete=models.DO_NOTHING)
+    business_id = models.ForeignKey(business, on_delete=models.DO_NOTHING)
+    text = models.TextField()
+    date = models.DateTimeField('date published', null=True, default=None)
+    likes = models.IntegerField(null=True, default=None)
